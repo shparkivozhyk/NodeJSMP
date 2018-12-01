077311b3928a37bd0f8fb8a44ad5308466f3442d
Maryna Kavetskaya <m.kovetskaya@gmail.com>
2018-12-01 23:39:57 +0300

    add homework 1 implementation

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import config from "./config";
+import { Product, User } from "./models";
+
+console.log((<any>config).name);
+const a = {
+  f: 30,
+};
+const user = new User();
+const product = new Product();
