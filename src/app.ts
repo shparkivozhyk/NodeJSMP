7ff76e2f09f22ca458dec29e9076377ff2707872
Maryna Kavetskaya <m.kovetskaya@gmail.com>
2018-12-04 11:25:56 +0300

    test push

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import config from "./config/config.json";
 import { Product, User } from "./models";
 
 console.log(config.name);
-console.log('hi there');
+console.log('hi there, you');
 const user = new User();
 const product = new Product();
