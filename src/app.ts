688873a306afb8bc5794cce4056f7916c1cbf4fe
Maryna Kavetskaya <m.kovetskaya@gmail.com>
2018-12-04 11:54:18 +0300

    new test

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import config from "./config/config.json";
 import { Product, User } from "./models";
 
 console.log(config.name);
-console.log('hi there, again and again');
+console.log('hi there, again and again')
 const user = new User();
 const product = new Product();
