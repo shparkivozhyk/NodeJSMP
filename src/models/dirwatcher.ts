81698cf755f3a6d1f1bc94ffbd1057977b1c39cd
Maryna Kavetskaya <m.kovetskaya@gmail.com>
2018-12-20 01:48:14 +0300

    add checking files for deletion and modifications

--- src/models/dirwatcher.ts
+++ src/models/dirwatcher.ts
@@ -4,35 +4,74 @@ import _ from "lodash";
 import { promisify } from "util";
 
 const readDir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
+
+enum FileStatus {
+  ADDED = "added",
+  MODIFIED = "modified",
+  DELETED = "deleted",
+ }
 
 export class DirWatcher extends EventEmitter {
   timerId: any;
   files: string[];
+  fileTempStatistics: object;
+  fileStatistics: object;
 
   constructor() {
     super();
     this.timerId = undefined;
     this.files = [];
+    this.fileStatistics = {};
+    this.fileTempStatistics = {};
+  }
+
+  checkStatus(file: string): object {
+    const fileLastModified = this.fileStatistics[file],
+          fileTimestamp = this.fileTempStatistics[file];
+
+    if (!fileLastModified) {
+      return {[file]: FileStatus.ADDED, };
+    } else if (!fileTimestamp) {
+      return {[file]: FileStatus.DELETED, };
+    } else if (fileTimestamp !== fileLastModified) {
+      return {[file]: FileStatus.MODIFIED, };
+    }
   }
 
   checkDir(path: any) {
-    readDir(path)
+    return readDir(path)
       .then((files) => {
-        const newFiles = _.difference(files, this.files);
-        this.files = files;
-        if (newFiles.length) {
-          this.emit("change", {path, newFiles, });
-        }
-      });
+          const fileTempStatistics = files.map(file => stat(`${path}/${file}`)
+                .then(stat => {
+                  this.fileTempStatistics[`${path}/${file}`] = stat.mtime.valueOf();
+                  return `${path}/${file}`;
+                }));
+
+          Promise.all(fileTempStatistics)
+            .then((files) => {
+              let changedFiles = {};
+              _.union(files, _.keys(this.fileStatistics))
+                .map(file => {
+                  changedFiles = _.assign(changedFiles, this.checkStatus(file));
+                });
+
+              this.fileStatistics = _.assign({}, this.fileTempStatistics);
+              this.fileTempStatistics = {};
+              if (!_.isEmpty(changedFiles)) {
+                this.emit("change", changedFiles);
+              }
+            });
+        });
   }
 
-  watch(path: any, delay: number) {
+  watch(path: any, delay: number): void {
     this.timerId = setInterval(() => {
       this.checkDir(path);
     }, delay);
   }
 
-  unwatch() {
+  unwatch(): void {
     this.timerId.unref();
   }
 }
--- src/models/importer.ts
+++ src/models/importer.ts
@@ -1,40 +1,58 @@
 import { EventEmitter } from "events";
 import fs from "fs";
+import _ from "lodash";
 import Papa from "papaparse";
 import { promisify } from "util";
 
+interface LoggingData {
+  file: string;
+  action: string;
+  csv: string;
+}
+
 export class Importer {
-  eventEmitter: EventEmitter;
-  async: boolean;
 
-  constructor (eventEmitter: EventEmitter, async: boolean = true) {
-    this.eventEmitter = eventEmitter;
-    this.async = async;
-  }
+  constructor (private eventEmitter: EventEmitter, private async: boolean = true) {}
 
-  import (path: string): PromiseLike<any> {
+  import (path: string): Promise<any> {
     const readDir = promisify(fs.readFile);
     return readDir(path, "utf8");
   }
 
-  importSync (path: string) {
+  importSync (path: string): string {
     return fs.readFileSync(path, "utf8");
   }
 
-  csvToJson (csv: string) {
+  csvToJson (csv: string): object {
     return Papa.parse(csv, {header: true, });
   }
 
-  listen () {
+  logData (data: LoggingData ): void {
+    console.log(`The file ${data.file} has been ${data.action}, data is `,
+                this.csvToJson(data.csv));
+  }
+
+  listen (): void {
     this.eventEmitter.on("change", (data) => {
-      data.newFiles.map(file => {
+      _.keys(data).map(file => {
         if (this.async) {
-          this.import(`${data.path}/${file}`)
+          this.import(file)
             .then(csv => {
-              console.log(this.csvToJson(csv));
+              this.logData({file, action: data[file], csv, });
+            })
+            .catch(error => {
+              if (error.code === "ENOENT") {
+                this.logData({file, action: data[file], csv: "[]", });
+              }
             });
         } else {
-          console.log(this.csvToJson(this.importSync(`${data.path}/${file}`)));
+          let csv: string;
+          try {
+            csv = this.importSync(file);
+          } catch {
+            csv = "[]";
+          }
+          this.logData({file, action: data[file], csv, });
         }
       });
     });
