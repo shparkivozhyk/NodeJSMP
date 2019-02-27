acce51719f404078ae3d525933994a0b5030a471
Maryna Kavetskaya <Maryna.Kavetskaya@burberry.com>
2019-02-27 13:37:38 +0300

    add user and product interfaces

--- src/express-server/controllers/products.ts
+++ src/express-server/controllers/products.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { db } from "../config/db";
 import { ErrorMessages } from "../constants/ErrorMessages";
+import { Product } from "../interfaces";
 
 export const getProduct = (req: Request, res: Response) => {
   db.products.findById(req.params.product_id)
-    .then((product: object) => {
+    .then((product: Product) => {
       if (!product) {
         res.send(ErrorMessages.NOPRODUCT);
       }
@@ -15,7 +16,7 @@ export const getProduct = (req: Request, res: Response) => {
 
 export const  getProducts = (req: Request, res: Response) => {
   db.products.findAll()
-    .then((products: object) => res.send(JSON.stringify(products)))
+    .then((products: Product) => res.send(JSON.stringify(products)))
     .catch((error: object) => res.send(ErrorMessages.NOPRODUCTS));
 };
 
@@ -23,13 +24,13 @@ export const createProduct = (req: Request, res: Response) => {
   const { title, price, id, reviews, } = req.body;
 
   db.products.create({ title, price, id, reviews, })
-    .then((product: object) => res.json(product))
+    .then((product: Product) => res.json(product))
     .catch((error: object) => console.log("Error:", error));
 };
 
 export const getReviews = (req: Request, res: Response) => {
   db.products.findById(req.params.product_id)
-    .then((product: object) => {
+    .then((product: Product) => {
       if (!product) {
         res.send(ErrorMessages.NOPRODUCT);
       }
