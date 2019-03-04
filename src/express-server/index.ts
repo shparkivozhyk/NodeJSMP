798dbf9a905b70283fe680296194b5f3da8097ab
Maryna Kavetskaya <Maryna.Kavetskaya@burberry.com>
2019-03-04 18:56:55 +0300

    add db cli param

--- src/express-server/index.ts
+++ src/express-server/index.ts
@@ -1,21 +1,6 @@
 import​​ app  ​from​​​ "./app"​;
-import { db } from "./config/db";
 import defaultConfig from "./config/defaultConfig.json";
-import { mongodb } from "./config/mongodb";
-
+import { connectDbToApp } from "./helpers";
 const​​ appPort = process.env.PORT || defaultConfig.defaultPort​;
 
-mongodb.on("open", () => {
-  console.log("Connection has been established successfully.");
-  app.listen(appPort, () => ​console​.log(​`App listening on port ​${appPort}​!`​));
-});
-mongodb.on("error", (err) => console.error("Unable to conner to the database", err));
-
-db.sequelize
-  .sync()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch(err => {
-    console.error("Unable to connect to the database:", err);
-  });
+connectDbToApp(app, appPort);
